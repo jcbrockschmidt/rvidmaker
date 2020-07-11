dbc0a4896f838d33a6c5fb2808cd8c2dacec1f26
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-07-11 15:14:05 -0700

    Add method for getting the best comments from an article

--- rvidmaker/readers/__init__.py
+++ rvidmaker/readers/__init__.py
@@ -1,3 +1,2 @@
 class reddit:
-    from .reddit import ConfigNotFound, RedditApiException
     from .reddit import RedditArticle, RedditReader
--- rvidmaker/readers/reddit.py
+++ rvidmaker/readers/reddit.py
@@ -1,27 +1,65 @@
 """Provides objects for parsing subreddits articles"""
 
+from copy import copy
 from datetime import datetime
 import json
 import os
 import praw
 
+from rvidmaker.exceptions import ConfigNotFound, RedditApiException
+
 CONFIG_PATH = 'config.json'
 USER_AGENT = 'rvidmaker 0.0.1'
 
-class ConfigNotFound(Exception):
-    """Raised when no config file is found"""
+class RedditComment:
+    """Represents a comment to a Reddit article"""
 
-class RedditApiException(Exception):
-    """Raised when no config file is found"""
-    def __init__(self, msg):
-        super().__init__(msg)
+    def __init__(self, author, text, score):
+        """
+        Args:
+            text: The comment's text.
+        """
+        self.author = author
+        self.text = text
+        self.score = score
+        self._child = None
+
+    @staticmethod
+    def from_praw(praw_comment):
+        """
+        Args:
+            praw_comment (praw.models.reddit.comment.Comment): PRAW generated comment.
+        """
+        author = praw_comment.author
+        text = praw_comment.body
+        score = praw_comment.score
+        return RedditComment(author, text, score)
+
+    def set_child(self, child):
+        """
+        Sets the child comment for a comment. Can only set one child.
+
+        Args:
+            child (RedditComment): The child comment. None to set no child.
+        """
+        self._child = None
+
+    def get_child(self):
+        """
+        Gets the child comment if one exists.
+
+        Returns:
+            (RedditComment): The child comment, if it exists.
+            (None): If there is no child.
+        """
+        return copy(self._child)
 
 class RedditArticle:
     """Represents a Reddit article"""
 
     def __init__(self, praw_article):
         """
-        Arguments:
+        Args:
             praw_article (praw.models.reddit.submission.Submission): The original PRAW generated
                 article.
         """
@@ -47,8 +85,61 @@ class RedditArticle:
         hours = (curtime - self._time_created) / (60 * 60)
         return hours
 
-    def get_comments(self):
-        raise NotImplementedError
+    def _expand_comment(self, comment, praw_comment, max_depth, percent_thres):
+        if max_depth <= 0:
+            return
+
+        # Get highest-scored reply
+        best_reply = None
+        for reply in praw_comment.replies:
+            if not isinstance(reply, praw.models.reddit.comment.Comment):
+                continue
+            if best_reply is None:
+                best_reply = reply
+            elif reply.score > best_reply.score:
+                best_reply = reply
+
+        if not best_reply is None:
+            if best_reply.score > comment.score * percent_thres:
+                child = RedditComment.from_praw(best_reply)
+                comment.set_child(child)
+                self._expand_comment(child, best_reply, max_depth - 1, percent_thres)
+
+    def get_comments(self, max_comments=10, max_depth=2, percent_thres=0.5):
+        """
+        Gets the best comments.
+
+        Args:
+            max_comments (int): Maximum number of comments to return.
+            max_depth (int): Maximum depth of comments to expand to.
+            percent_thres (float): What proportion of a parent comment's score a child comment must
+                have to be included.
+
+        Returns:
+            (list) List of `RedditComment`s.
+        """
+        max_comments = max(1, max_comments)
+        max_depth = max(0, max_depth)
+        percent_thres = max(0, percent_thres)
+
+        # Sort comments by score (descending order)
+        praw_comments = []
+        for comment in self._article.comments:
+            if not isinstance(comment, praw.models.reddit.comment.Comment):
+                continue
+            praw_comments.append(comment)
+        praw_comments.sort(key=lambda x: x.score, reverse=True)
+
+        cnt = 0
+        comments = []
+        for praw_comment in praw_comments[:max_comments]:
+            comment = RedditComment.from_praw(praw_comment)
+            if comment.author is None or comment.text == '[deleted]':
+                continue
+            self._expand_comment(comment, praw_comment, max_depth, percent_thres)
+            comments.append(comment)
+
+        return comments
 
 class RedditReader:
     """Reads popular articles from a subreddit"""
