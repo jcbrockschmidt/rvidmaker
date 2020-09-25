30e49ca6b3cf58e197f0df5b8471fa234a3510d7
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-09-25 06:02:21 -0700

    Add title text to split thumbnail

--- rvidmaker/thumbnails/split.py
+++ rvidmaker/thumbnails/split.py
@@ -1,10 +1,7 @@
 """Implements function for creating a split thumbnail of a single video"""
 
 from moviepy.editor import afx, CompositeVideoClip, concatenate_videoclips, TextClip, VideoFileClip
-from PIL import Image
-
-# Size of a thumbnail.
-SIZE = (1280, 720)
+from PIL import Image, ImageDraw, ImageFont
 
 def _make_pane(img, size):
     """
@@ -31,23 +28,61 @@ def _make_pane(img, size):
     pane = img.resize((sw, sh), box=box)
     return pane
 
-def create_split_thumbnail(video_path):
+def create_split_thumbnail(
+        video_path, title,
+        size=(1280, 720),
+        font_size=150,
+        font_color=(255, 255, 255),
+        box_fill=(255, 69, 0),
+        padding=20,
+        text_rotate=5
+):
     """
     Creates a split thumbnail from a single video. A frame early in the video is placed next to a
     frame later in the video.
 
     Args:
         video_path (str): Path to a video to generate thumbnail with.
+        title (str): Title to place on thumbnail.
+        size (int, int): Width and height of thumbnail.
+        font_size (int): Font size of title text.
+        font_color (int, int, int): RGB color of the title text.
+        box_fill (int, int, int): RGB color of the box around the title text.
+        padding (int): Padding of the box around the title text in pixels.
+        text_rotate (float): Degrees to rotate the title text.
     """
+    w, h = size
+
+    # Get two frames and place them side-by-side.
     clip = VideoFileClip(video_path)
     lt_t = clip.duration * 0.2
     rt_t = clip.duration * 0.5
     lt_frame = Image.fromarray(clip.get_frame(lt_t))
     rt_frame = Image.fromarray(clip.get_frame(rt_t))
-    lt_pane = _make_pane(lt_frame, SIZE)
-    rt_pane = _make_pane(rt_frame, SIZE)
-    final = Image.new('RGB', SIZE)
+    lt_pane = _make_pane(lt_frame, size)
+    rt_pane = _make_pane(rt_frame, size)
+    final = Image.new('RGBA', size)
     final.paste(lt_pane, (0, 0))
-    xoffset = int(SIZE[0] / 2)
+    xoffset = int(w / 2)
     final.paste(rt_pane, (xoffset, 0))
+
+    # Create title frame.
+    font = ImageFont.truetype('Impact', size=font_size)
+    txt_w, txt_h = font.getsize(title)
+    yoffset = int(-font_size / 6)
+    title_size = (txt_w + padding * 2, txt_h + padding * 2 + yoffset)
+    title_img = Image.new('RGB', title_size, color=box_fill)
+    draw = ImageDraw.Draw(title_img)
+    draw.text((padding, padding + yoffset), title, font=font, fill=font_color)
+
+    # Rotate text and create alpha mask.
+    title_rot = title_img.rotate(text_rotate, expand=True)
+    mask = Image.new('RGB', title_img.size, (255, 255, 255))
+    mask = mask.rotate(text_rotate, expand=True).convert('L').resize(title_rot.size)
+
+    # Place title frame in center.
+    tw = title_size[0]
+    pos = (int((w - tw) / 2), 20)
+    final.paste(title_rot, pos, mask)
+
     return final
