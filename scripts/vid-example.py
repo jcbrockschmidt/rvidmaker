1089b79de18160d76dbfa43a9460a56de2dbb158
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-09-26 20:41:34 -0700

    Add thumbnail creation to example video creation script

--- scripts/vid-example.py
+++ scripts/vid-example.py
@@ -2,31 +2,79 @@
 
 """Creates a compilation of top videos from r/dankvideos"""
 
-import rvidmaker
+import os
 from rvidmaker.editor import VideoCompiler
+from rvidmaker.readers.reddit import RedditReader
+from rvidmaker.thumbnails import create_split_thumbnail
+from rvidmaker.utils import shorten_title
 
 SUBREDDIT = 'dankvideos'
-OUTPUT = 'example.mp4'
+OUTPUT_DIR = 'output'
+VID_PATH = os.path.join(OUTPUT_DIR, 'video.mp4')
+THUMB_PATH = os.path.join(OUTPUT_DIR, 'thumbnail.png')
 
 MIN_SCORE = 100
-MIN_AGE = 0
 MAX_DURATION = 40
 MIN_DURATION = 2
 LIMIT = 100
 SIZE = (1920, 1080)
+MAX_VIDEOS = 50
+MAX_TITLE_LEN = 20
+
+def get_videos_from_reddit(subreddit, max_videos=None):
+    """
+    Gets videos from a subreddit.
+
+    Args:
+        subreddit (str): Subreddit to get articles from.
+        max_videos (int): Maximum number of videos to gather. None if no maximum.
+
+    Returns:
+        list: List of `rvidmaker.videos.VideoRef` in ascending order of score.
+    """
+    reader = RedditReader()
+    articles = reader.get_top_articles(SUBREDDIT, time_filter='week', limit=LIMIT, min_score=MIN_SCORE)
+    videos = []
+    for art in articles:
+        if not art.nsfw and art.has_video(min_duration=MIN_DURATION, max_duration=MAX_DURATION, include_youtube=False):
+            videos.append(art.get_video())
+            if not max_videos is None:
+                if len(videos) >= max_videos:
+                    break
+    return videos
+
+def make_thumbnail(vid, output_path):
+    """
+    Creates a thumbnail from a single video.
+
+    Args:
+        vid (rvidmaker.videos.VideoRef): Video to create thumbnail from.
+        output_path (str): Path to write the thumbnail to.
+    """
+    short_title = shorten_title(vid.get_title(), MAX_TITLE_LEN)
+    temp_vid_dl = vid.download(os.path.join(OUTPUT_DIR, 'temp-video'))
+    thumb = create_split_thumbnail(temp_vid_dl, short_title)
+    thumb.save(output_path)
+    os.remove(temp_vid_dl)
+
+def main():
+    videos = get_videos_from_reddit(SUBREDDIT, MAX_VIDEOS)
+    if len(videos) < 2:
+        print('Not enough videos gathered for a compilation')
+        return
+
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+
+    print('Rendering compilation of {} videos...'.format(len(videos)))
+    compiler = VideoCompiler()
+    for v in videos:
+        compiler.add_video(v)
+    compiler.render_video(SIZE, VID_PATH)
+
+    print("Creating thumbnail...")
+    # We use the top-scored video to create our thumbnail.
+    make_thumbnail(videos[0], THUMB_PATH)
 
 if __name__ == '__main__':
-    reader = rvidmaker.readers.reddit.RedditReader()
-    articles = reader.get_top_articles(SUBREDDIT, time_filter='week', limit=LIMIT, min_score=MIN_SCORE)
-    vid_num = 0
-    videos = []
-    compiler = VideoCompiler()
-    for art in articles:
-        if not art.nsfw and art.has_video(max_duration=MAX_DURATION, include_youtube=False):
-            compiler.add_video(art.get_video())
-    vid_count = compiler.get_video_count()
-    if vid_count < 2:
-        print('Not enough videos gathered for a compilation')
-    else:
-        print('Rendering compilation of {} videos...'.format(compiler.get_video_count()))
-        compiler.render_video(SIZE, OUTPUT)
+    main()
