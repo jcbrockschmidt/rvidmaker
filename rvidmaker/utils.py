ec167725b9f279f4cecc296a634cce621e4d04b5
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-09-26 20:33:28 -0700

    Add utility for shortening titles

--- rvidmaker/utils.py
+++ rvidmaker/utils.py
@@ -1,5 +1,7 @@
 import os
+from rake_nltk import Rake
 import random
+import re
 
 _CHAR_LIST = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
@@ -38,3 +40,47 @@ def get_random_path(root, ext):
         path = os.path.join(root, '{}.{}'.format(rand_str, ext))
         if not os.path.exists(path):
             return path
+
+def shorten_title(title, max_title_len, alphanum_only=True):
+    """
+    Shortens a title using important phrases and keywords in the title.
+
+    Args:
+        title (str): Title to shorten.
+        max_title_len (int): Maximum length of the final title.
+        alphanum_only (bool): Whether to only use alphanumeric characters.
+
+    Returns:
+        str: Shortened, all lower-case title with a length less than `max_title_len`.
+    """
+    title = title.lower()
+    if alphanum_only:
+        filter = re.compile('[^a-z0-9 ]')
+        title = filter.sub('', title)
+
+    if len(title) <= max_title_len:
+        # Title is already short enough.
+        return title
+
+    # Try using the highest ranked phrase from the title.
+    r = Rake()
+    r.extract_keywords_from_text(title)
+    new_title = r.get_ranked_phrases()[0]
+    if len(new_title) <= max_title_len:
+        return new_title
+
+    # Title is still too long. Use as many of the important words as will fit within the max
+    # title length.
+    words = sorted(r.get_word_degrees())
+    new_title = words[0]
+    if len(new_title) > max_title_len:
+        # Cut the single-word title short.
+        return new_title[:max_title_len]
+
+    for w in words[1:]:
+        append_title = '{} {}'.format(new_title, w)
+        if len(append_title) > max_title_len:
+            break
+        new_title = append_title
+
+    return new_title
